f284cf1f5c925d6dc45d1194c906bf5cad8b8087
paleblue-digital <admin@paleblue.digital>
2023-10-02 19:16:57 -0700

    App class and stubs done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      imagePath: 'Cards/',
+      firstPick: -1,
+      secondPick: -1,
+      matches: 0,
+      tries: 0,
+      images: this.fillImages(),
+      
+    };
+    this.shuffleImages(this.state.images);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.checkCards = this.checkCards.bind(this);
+    this.isMatch = this.isMatch.bind(this);
+
+
+  }
 }
 
 export default App;
