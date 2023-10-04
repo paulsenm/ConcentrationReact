1b18aa8be2dacf00ef967ae0e6f070a33d215c69
paleblue-digital <admin@paleblue.digital>
2023-10-04 09:07:58 -0700

    Still working on converting to react - not working yet

--- src/App.js
+++ src/App.js
@@ -24,35 +24,138 @@ class App extends Component {
 
   fillImages(){
     console.log("Images filled.");
+    let images = Array(20).fill(null);
+    let values = ['a', 'k', 'q', 'j', 't', '9', '8', '7', '6', '5'];
+    let suits = ['h', 's'];
+    let index = 0;
+    for (let value = 0; value < values.length; value++){
+        for (let suit = 0; suit < suits.length; suit ++) {
+            images[index] = "card" + values[value] + suits[suit] + ".jpg";
+            index++;
+        }
+    }
+    return images;
   }
 
   shuffleImages(images){
     console.log("Images shuffled.");
+    for (let i = 0; i < images.length; i++) {
+      let rnd = Math.floor(Math.random() * images.length);
+      [images[i], images[rnd]] = [images[rnd], images[i]];
+  }
   }
 
   renderCard(i){
-    console.log("Card rendered.")
+    console.log("Card rendered.");
+    const image = (this.state.images[i] == null) ? 'none' : 
+    ( this.state.firstPick == i || this.state.secondPick == i) ? 
+    'url(' + this.state.imagePath + this.state.images[i] + ')' : 
+    'url(' + this.state.imagePath + 'black_back.jpg)';
+    const enabled = (this.state.images[i] != null && 
+    (i != this.state.firstPick && i != this.state.secondPick) &&
+    (this.state.firstPick == -1 || this.state.secondPick == -1) &&
+    (this.state.matches < 10)) ? true : false;
+    const eventHandler = (enabled) ? this.handleClick(i) : () => {};
+    const cursor = (enabled) ? "pointer" : "none";
+    const output = 
+      <div 
+          id={i} key={i} 
+          name="card" 
+          className="col-sm-2 card"
+          style={{backgroundImage: image, cursor: cursor}}
+          onClick={eventHandler}
+          >
+          &nbsp;
+      </div>
+;
+return output;
   }
 
   handleClick(event){
+    let index = 1;
     console.log("Click handled.");
+    if (event.target){
+      index = parseInt(event.target.id);
+    }
+   
+
+    if (this.state.firstPick == -1)
+        this.state.firstPick = index;
+    else {
+        this.state.secondPick = index;
+        setTimeout(this.checkCards, 2000);
+    }
   }
 
   isMatch(){
     console.log("Match is'ed.");
+    if (this.state.images[this.state.firstPick].substr(4, 1) == this.state.images[this.state.secondPick].substr(4, 1))
+    return true;
+else
+    return false;
   }
 
   checkCards(){
     console.log("Cards checked.");
+    this.state.tries++;
+    if (this.isMatch()) {
+        this.state.matches ++;
+        this.state.images[this.state.firstPick] = null;
+        this.state.images[this.state.secondPick] = null;
+    }
+    this.state.firstPick = -1;
+    this.state.secondPick = -1;
   }
 
   render(){
     console.log("Rendered.");
-    let output = 
-      <div>
-        Rendered
-      </div>
-      ;
+    let status = (this.state.matches < 10) ?
+      'Matches: ' + this.state.matches + " Tries: " + this.state.tries :
+      "Congratulations!  You found all 10 matches in " + this.state.tries + " tries!";
+
+const output = 
+       <div class="container" id="board">
+            <div class="pb-2" id="status">{status}</div>
+            <div class="row">
+                <div class="col-sm-1"></div>
+                {this.renderCard(0)}
+                {this.renderCard(1)}
+                {this.renderCard(2)}
+                {this.renderCard(3)}
+                {this.renderCard(4)}
+                <div class="col-1"></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-1"></div>
+                {this.renderCard(5)}
+                {this.renderCard(6)}
+                {this.renderCard(7)}
+                {this.renderCard(8)}
+                {this.renderCard(9)}
+                <div class="col-1"></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-1"></div>
+                {this.renderCard(10)}
+                {this.renderCard(11)}
+                {this.renderCard(12)}
+                {this.renderCard(13)}
+                {this.renderCard(14)}
+                <div class="col-1"></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-1"></div>
+                {this.renderCard(15)}
+                {this.renderCard(16)}
+                {this.renderCard(17)}
+                {this.renderCard(18)}
+                {this.renderCard(19)}
+                <div class="col-1"></div>
+            </div>
+        </div>
+    
+    document.getElementById("top").innerHTML = output;
+
 
     return output;
   }
